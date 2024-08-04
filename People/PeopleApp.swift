19c16fb70464e21e1f659e14b209de3123687bfc
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-04 06:41:57 +0200

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Adam Elfsborg on 2024-08-04.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
